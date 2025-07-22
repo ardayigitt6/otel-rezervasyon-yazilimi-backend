a55c6fa4e9c58246fbde2494cbf5097a35bdd135
Arda Yiğit <arda.yigit.5811@gmail.com>
2025-07-22 18:39:51 +0300

    some update

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-require('dotenv').config();
 const express = require ('express') ;
-const mysql = require ('mysql2');
 const cors = require('cors');
+require('dotenv').config();
+
+const userController = require('./controllers/userController');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
-db.connect ((err) =>{
-if(err) {
-    console.error('MySQL bağlantı hatası:',err);
-    process.exit(1);
-}
-console.log('MySQL bağlantısı başarılı!');
-});
-
 app.get('/',(req,res)=>{
     res.send('Otel Rezervasyonu Yazılımı Backendi çalışıyor.!');
 });
+
+app.post('/register',userController.register);
+app.post('login',userController.login);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
