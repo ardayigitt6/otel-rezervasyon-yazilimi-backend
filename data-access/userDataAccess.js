9aad727ebab23690067a9505542a3b919ab47ba5
Arda Yiğit <arda.yigit.5811@gmail.com>
2025-07-22 19:09:51 +0300

    fix the some lines

--- data-access/userDataAccess.js
+++ data-access/userDataAccess.js
@@ -1,18 +1,28 @@
 const db = require('../db');
 
 const createUser = (user, callback) => {
-    const sql = 'INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)';
-    db.query(sql, [user.name, user.email, user.password, user.role], callback);
+    const sql = 'INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)';
+    db.query(sql, [user.name, user.email, user.password, user.role], function (err, result) {
+        callback(err, result);
+    });
 };
 
 const findUserByEmail = (email, callback) => {
-    const sql = 'SELECT*FROM users WHERE email =?';
-    db.query(sql, [email], callback);
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    db.query(sql, [email], function (err, results) {
+        callback(err, results);
+    });
 };
 
 const findUserByEmailAndPassword = (email, password, callback) => {
-    const sql = 'SELECT*FROM users WHERE email=? AND password =?';
-    db.query(sql, [email], [password], callback);
+    const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    db.query(sql, [email, password], function (err, results) {
+        callback(err, results);
+    });
 };
 
-module.exports = { createUser, findUserByEmail, findUserByEmailAndPassword };
+module.exports = {
+    createUser,
+    findUserByEmail,
+    findUserByEmailAndPassword
+};
